8f11c0d68486d1fa5ed0183f46ae6412e54da4b9
lrogowski93 <lrogowski93@gmail.com>
2025-03-13 21:30:31 +0100

    Initial commit

---
+++ src/main/java/com/demo/easyuploader_pick_pack_assistant/EasyUploaderPickPackAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.demo.easyuploader_pick_pack_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyUploaderPickPackAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyUploaderPickPackAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/easyuploader_pick_pack_assistant/EasyUploaderPickPackAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.easyuploader_pick_pack_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyUploaderPickPackAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
